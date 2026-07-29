ee895a77c6c4fe7a50c462e97c4b0bdc7b62e3d7
abxh <83485102+abxh@users.noreply.github.com>
2026-07-29 19:43:54 +0200

    patch redundant macros and warnings

--- lib/types/strtable.h
+++ lib/types/strtable.h
@@ -5,8 +5,6 @@
 #define NAME       strtable
 #define KEY_TYPE   struct strview
 #define VALUE_TYPE struct strview
-#define KEY_IS_EQUAL
-#define HASH_FUNCTION
 #define TYPE_DEFINITIONS
 #include <data-structures-c/fhashtable/fhashtable_template.h>
 
